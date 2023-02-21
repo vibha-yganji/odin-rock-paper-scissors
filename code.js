3aef6c05df96b9ecfc66228c5ccb9d8b5c3d483e
Vibha <vibhayadavganji@gmail.com>
2023-02-20 20:51:30 -0800

    Create JS function to return random choice of rock, paper, or scissors

---
+++ code.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let numerical_choice = Math.floor(Math.random() * 2);
+    switch (choice) {
+        case 0:
+            let choice = 'Rock';
+        case 1:
+            choice = 'Scissor';
+        case 2:
+            choice = 'Paper';
+
+    } 
+}
