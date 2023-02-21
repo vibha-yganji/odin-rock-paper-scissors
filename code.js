5142d0fa5a2cc572b1f2c20f1fd4d77cb5749ea6
Vibha <vibhayadavganji@gmail.com>
2023-02-20 20:54:12 -0800

    Corrected expression in switch statement to numerical_choice variable

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let numerical_choice = Math.floor(Math.random() * 2);
-    switch (choice) {
+    switch (numerical_choice) {
         case 0:
             let choice = 'Rock';
         case 1:
