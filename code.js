2d1ba70441977be46008d87d9f6b436d80afa49a
Vibha <vibhayadavganji@gmail.com>
2023-02-20 21:31:18 -0800

    Add a function to play a single round between computer and player

--- code.js
+++ code.js
@@ -12,3 +12,42 @@ function getComputerChoice() {
             break;
     } 
 }
+
+function play_round (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == 'rock') {
+        if (computerSelection.toLowerCase() == 'scissors') {
+            return 'You win. Rock beats Scissors'
+        }
+        if (computerSelection.toLowerCase() == 'rock') {
+            return 'Tie';
+        }
+        if (computerSelection.toLowerCase() == 'paper') {
+            return "You lose. Paper beats Rock"
+        }
+    }
+    if (playerSelection.toLowerCase() == 'paper') {
+        if (computerSelection.toLowerCase() == 'scissors') {
+            return 'You lose. Scissors beats paper'
+        }
+        if (computerSelection.toLowerCase() == 'rock') {
+            return 'You win. Paper beats Rock'
+        }
+        if (computerSelection.toLowerCase() == 'paper') {
+            return 'Tie!'
+        }
+    }
+    if (playerSelection.toLowerCase() == 'scissors') {
+        if (computerSelection.toLowerCase() == 'scissors') {
+            return 'Tie!'
+        }
+        if (computerSelection.toLowerCase() == 'rock') {
+            return 'You lose. Rock beats Scissors'
+        }
+        if (computerSelection.toLowerCase() == 'paper') {
+            return 'You win. Scissors beats paper'
+        }
+        
+    
+    }
+
+}
